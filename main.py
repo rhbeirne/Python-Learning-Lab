e46a0fa321b5dff13933a2408b02caeae2d72ef9
rhbeirne <125299299+rhbeirne@users.noreply.github.com>
2026-03-02 17:39:58 +0000

    Fixed Boss class inheritance nd initialized main script

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from scripts.VRPlayer import VRPlayer
 from scripts.enemies import Boss
 
-p1 = VRPlayer("Richard")
-b1 = Boss("Ganon", 99)
+p1 = VRPlayer("Richard", [0, 0, 0], 100)
+b1 = Boss("Ganon",[5,5,5],500 ,99)
 
 b1.show_inventory() # Notice: Boss can use Player methods!
--- scripts/enemies.py
+++ scripts/enemies.py
@@ -1,10 +1,10 @@
 from scripts.VRPlayer import VRPlayer
 
 class Boss(VRPlayer):
-    def __init__(self, name, power_level):
-        # Run the standard Player setup
-        super().__init__(name) 
-        # Add the unique Boss traits
+    def __init__(self, name, position, health, power_level):
+        # 1. Pass the name, position, and health up to the VRPlayer setup
+        super().__init__(name, position, health)
+        
+        # 2. Set the unique Boss trait
         self.power_level = power_level
-        self.health = 500
-        print(f"Boss {self.name} has entered the game with {self.health} HP!")
+        print(f"Boss {self.name} has entered with {self.health} HP and Power Level {self.power_level}!")
