0cee77f9a6a5322a26653c19d49b7d4ef2108184
rhbeirne <125299299+rhbeirne@users.noreply.github.com>
2026-04-20 16:16:32 +0000

    Library exercises

---
+++ Library_exercises.py/main.py
@@ -0,0 +1,25 @@
+#Library
+
+print("--- Ticket-118: Fleet Report Generator ---")
+from datetime import datetime
+
+class ReportGenerator:
+
+    def __init__(self, filename):
+        self.filename = filename
+        
+    def write_report(self, offline_servers):
+        current_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+        with open(self.filename, "w") as file:
+            file.write(f"--- Offline Server Report: {current_time} ---\n")
+            for s in offline_servers:
+                file.write(s + "\n")
+
+# --- Testing the Generator ---
+# Here is a mock list of offline servers
+crashed_servers = ["DB-01", "Mail-Gateway", "Auth-Server"]
+
+my_reporter = ReportGenerator("fleet_report.txt")
+my_reporter.write_report(crashed_servers)
+
+print("Script finished! Check your local folder for the new file.")
